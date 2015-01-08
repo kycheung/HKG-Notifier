18eda2d4fe7ffd67062640196be79ef23c38c1dd
Donald Cheung <kycheung@cse.cuhk.edu.hk>
2015-01-08 14:29:07 +0800

    First Draft

---
+++ background.js
@@ -0,0 +1,64 @@
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		var storage = chrome.storage.sync;
+		if (request.action == "bookmark") {
+			var url = request.url;
+			var post_id = url.match(/http:\/\/.+hkgolden\.com\/.+message=([0-9]+)/)[1];
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url, false);
+			xhr.send(null);
+			if(xhr.status == 200) {
+				var title = xhr.responseText.match(/<Attribute name="title">(.*?)<\/Attribute>/)[1];
+				var last_viewed_time = xhr.responseText.match(/<Attribute name="last_update">(.*?)<\/Attribute>/)[1];
+				var num_of_replies = xhr.responseText.match(/([0-9]+).+?個回應/)[1];
+				var object = {"url":url, "title":title, "last_viewed_time":last_viewed_time, "num_of_replies":num_of_replies, "new_reply":false, "post_id":post_id};
+				obj = {}; obj[post_id] = object;
+				storage.set(obj, function (){
+					sendResponse({msg: title + " Bookmarked", "post_id":post_id, "title":title, "last_viewed_time":last_viewed_time, "num_of_replies":num_of_replies, "url":url});
+				});
+			}
+		} else if (request.action == "query") {
+			storage.get(null, function(items) {
+				sendResponse(items);
+			});
+		} else if (request.action == "clear") {
+			storage.clear(function() {
+				sendResponse({msg : "All bookmarks are clear!"});
+			});
+		} else if (request.action == "refresh") {
+			storage.get(null, function(items) {
+				var keys = Object.keys(items);
+				for (var i = 0; i < keys.length; i++) {
+					var key = keys[i]; 
+					var post_id = items[key].post_id;
+					var url = items[key].url;
+					var title = items[key].title;
+					
+					var xhr = new XMLHttpRequest();
+					xhr.open('GET', url, false);
+					xhr.send(null);
+					if (xhr.status == 200) {
+						var last_viewed_time = xhr.responseText.match(/<Attribute name="last_update">(.*?)<\/Attribute>/)[1];
+						var num_of_replies = xhr.responseText.match(/([0-9]+).+?個回應/)[1];
+						var new_reply;
+						if (num_of_replies != items[key].num_of_replies) {new_reply = true;} else {new_reply = items[key].new_reply;}
+						console.log(new_reply);
+						var object = {"url":url, "title":title, "last_viewed_time":last_viewed_time, "num_of_replies":num_of_replies, "new_reply":new_reply, "post_id":post_id};
+						obj = {}; obj[post_id] = object;
+						storage.set(obj, function (){
+							;
+						});
+					}
+				}
+				sendResponse({msg : "Database Updated!"});
+			});
+		} else if (request.action == "removeOne") {
+			post_id = request.post_id;
+			console.log(post_id);
+			storage.remove(post_id, function() {
+				sendResponse({msg : "Post " + post_id + " is removed!"});
+			});
+		}
+		return true;
+	}
+);
---
+++ popup.js
@@ -0,0 +1,128 @@
+function addOne(post_id, title, last_view_time, num_of_replies, url, new_reply) {
+	var tr = document.createElement("tr");
+	tr.id = "row_" + post_id;
+	var td; var a; var button;
+	if (url == null) {
+		td = document.createElement("td");
+		td.appendChild(document.createTextNode(title));
+		tr.appendChild(td);
+	} else {
+		td = document.createElement("td");
+		a = document.createElement("a");
+		a.appendChild(document.createTextNode(title));
+		a.title = "Link to HKGolden Post"; a.href = url; a.target="_blank";
+		td.appendChild(a);
+		if (new_reply == true) {
+			var sup = document.createElement("sup");
+			sup.appendChild(document.createTextNode('NEW'));
+			td.appendChild(sup);
+		}
+		tr.appendChild(td);
+	}
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode(last_view_time));
+	tr.appendChild(td);
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode(num_of_replies));
+	tr.appendChild(td);
+	td = document.createElement("td");
+	button = document.createElement("input");
+	button.type = "button";
+	button.value = "Delete";
+	button.id = "button_" + post_id;
+	td.appendChild(button);
+	tr.appendChild(td);
+	document.getElementById("BookmarksTable").appendChild(tr);
+	document.getElementById("button_" + post_id).addEventListener('click', function () {
+		removeOne(post_id)
+	});
+}
+
+function addTitle() {
+	var tr = document.createElement("tr");
+	var td;
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode("題目"));
+	tr.appendChild(td);
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode("最後回應時間"));
+	tr.appendChild(td);
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode("回覆"));
+	tr.appendChild(td);
+	td = document.createElement("td");
+	td.appendChild(document.createTextNode("移除"));
+	tr.appendChild(td);
+	document.getElementById("BookmarksTable").appendChild(tr);
+}
+
+function bookmarkCurrent() {
+	chrome.tabs.query({"currentWindow" : true, active: true}, function (tabs) {
+		url = tabs[0].url;
+		var post_id = url.match(/http:\/\/.+hkgolden\.com\/.+message=([0-9]+)/);
+		if (post_id == null) {
+			alert("Current Tab is not a HKGolden Post!");
+		} else {
+			chrome.runtime.sendMessage({"action":"bookmark", "url": url}, function(response) {
+//				alert(response.msg);
+				addOne(response.post_id, response.title, response.last_viewed_time, response.num_of_replies, response.url);
+			});
+		}
+	});
+}
+
+function refreshAll() {
+	chrome.runtime.sendMessage({"action":"refresh"}, function(response) {
+		root = document.getElementById("BookmarksTable");
+		while (root.firstChild) {
+			root.removeChild(root.firstChild);
+		}
+		displayAll();
+		alert(response.msg);
+	});
+}
+
+function displayAll() {
+	chrome.runtime.sendMessage({"action":"query"}, function(response) {
+		result_object = response;
+		var keys = Object.keys(result_object);
+		addTitle();
+		for (var i = 0; i < keys.length; i++) {
+			var key = keys[i]; 
+			var post_id = result_object[key].post_id;
+			var last_view_time = result_object[key].last_viewed_time;
+			var title = result_object[key].title;
+			var url = result_object[key].url;
+			var num_of_replies = result_object[key].num_of_replies;
+			var new_reply = result_object[key].new_reply;
+			addOne(post_id, title, last_view_time, num_of_replies, url, new_reply);
+		}
+	});
+}
+
+function clearAll() {
+	chrome.runtime.sendMessage({"action":"clear"}, function(response) {
+		var myNode = document.getElementById("BookmarksTable");
+		while (myNode.firstChild) {
+			myNode.removeChild(myNode.firstChild);
+		}
+//		alert("All bookmarks are clear");
+		addTitle();
+	});
+}
+
+function removeOne(post_id) {
+	row_id = "row_" + post_id;
+	var element = document.getElementById(row_id);
+	element.parentNode.removeChild(element);
+	chrome.runtime.sendMessage({"action":"removeOne", "post_id":post_id}, function(response) {
+		;
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('BookmarkThis').addEventListener('click', bookmarkCurrent);
+	document.getElementById('RefreshAll').addEventListener('click', refreshAll);
+	document.getElementById('ClearAll').addEventListener('click', clearAll);
+	displayAll();
+});
